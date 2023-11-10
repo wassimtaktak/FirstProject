741bed79e96b843c5b9f90a9f2bcb8433086911d
wassimtaktak <wassimtaktak32@gmail.com>
2023-11-10 22:47:04 +0100

    examen

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class AuthorController extends AbstractController
 {   
     public $authors = array(
@@ -48,9 +53,76 @@ class AuthorController extends AbstractController
                 break; // Sortez de la boucle une fois que vous avez trouvé l'auteur
             }
         }    
-        return $this->render('author/showAuthor.html.twig', [
-            'id' => $id,'author' => $a,
+        return $this->render('author/showAuthor.html.twig', ['id' => $id,'author' => $a,]);
+    }
+    #[Route('/listauthors', name: 'list_authors')]
+    public function findallauthors(AuthorRepository $authorrepository): Response
+    {   
+        $auteurs = $authorrepository->findAll();
+        return $this->render('author/allauthors.html.twig', ['authors' => $auteurs,]);
+    }
+    #[Route('/auteur/add', name: 'author_ajout')]
+    public function addauthors(ManagerRegistry $doctrine,Request $request)
+    {   
+        $em=$doctrine->getManager();
+        $auteur = new Author();
+        /*$auteur->setUsername("wassim");
+        $auteur->setEmail("wassim@gmail.com");*/
+        $form = $this->createForm(AuthorType::class, $auteur);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            $em->persist($auteur);
+            $em->flush($auteur);
+            return $this->redirectToRoute('list_authors');
+        }
+        
+            return $this->renderForm('author/add.html.twig',['form'=>$form]);  
+    }
+    #[Route('/delete/{id}', name: 'app_author_delete')]
+    public function delete(int $id, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        // Recherchez l'auteur par son ID
+        $author = $entityManager->getRepository(Author::class)->find($id);
+
+        $entityManager->remove($author);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('list_authors');
+    }
+    #[Route('edit/{id}', name: 'app_author_edit')]
+    public function edit(Request $request,int $id, ManagerRegistry $doctrine): Response
+    {   
+        $entityManager=$doctrine->getManager();
+        $author = $entityManager->getRepository(Author::class)->find($id);
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('list_authors');
+        }
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Editer', // Personnalisez le label du bouton
+        ]);
+        return $this->renderForm('author/edit.html.twig', [
+            'author' => $author,
+            'form' => $form,
         ]);
     }
-
+    #[Route('/showordered', name: 'ordered_username')]
+    public function findauthorsorderbyusername(AuthorRepository $authorrepository): Response
+    {   
+        $auteurs = $authorrepository->triusername();
+        return $this->render('author/allauthors.html.twig', ['authors' => $auteurs,]);
+    }
+    #[Route('/showdql', name: 'dqlorder')]
+    public function findauthorsdql(AuthorRepository $authorrepository): Response
+    {   
+        $auteurs = $authorrepository->tridql();
+        return $this->render('author/allauthors.html.twig', ['authors' => $auteurs,]);
+    }
 }
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?Vol $vol = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getVol(): ?vol
+    {
+        return $this->vol;
+    }
+
+    public function setVol(?vol $vol): static
+    {
+        $this->vol = $vol;
+
+        return $this;
+    }
+}
