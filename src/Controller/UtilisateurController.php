1242ec6dd48995dd46a4ec90eb6c1e070df0ac39
selim abid <mohamedselim.abid@esprit.tn>
2024-04-16 11:42:14 +0100

    login+register

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -45,7 +45,7 @@ class UtilisateurController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_utilisateur_show', methods: ['GET'])]
+    #[Route('_id', name: 'app_utilisateur_show', methods: ['GET'])]
     public function show(Utilisateur $utilisateur): Response
     {
         return $this->render('utilisateur/show.html.twig', [
